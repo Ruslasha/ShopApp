09c8d800765ab11a45910e99cc2eec19d63edc6f
Ruslan <ruslan99992222@gmail.com>
2024-05-06 21:36:47 +0300

    Initial Commit

---
+++ ShopApp/ShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopAppApp.swift
+//  ShopApp
+//
+//  Created by Руслан Абрамов on 06.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
