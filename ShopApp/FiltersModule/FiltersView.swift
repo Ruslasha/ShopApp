3c5966c0e79ac882e65882eab15e7afda1596e55
Ruslan <ruslan99992222@gmail.com>
2024-05-12 14:28:24 +0300

    complete

--- ShopApp/FiltersModule/FiltersView.swift
+++ ShopApp/FiltersModule/FiltersView.swift
@@ -8,8 +8,173 @@
 import SwiftUI
 
 struct FiltersView: View {
+
+    enum Constants {
+        static let titleText = "Filters"
+        static let categoryText = "Category"
+        static let moreText = "More"
+        static let pricesText = "Prices"
+        static let colorText = "Color"
+        static let chevronLeft = "chevron.right"
+        static let chevronRight = "chevron.left"
+        static let minPriceText = "500$"
+    }
+
+    let columnsTypeFilters: [GridItem] = [
+        .init(.fixed(120))
+    ]
+
+    let columnsColorsFilters: [GridItem] = [
+        .init(.flexible()),
+        .init(.flexible()),
+        .init(.flexible()),
+        .init(.flexible()),
+        .init(.flexible()),
+    ]
+
+    @Environment(\.presentationMode) var presentation
+    var viewModel = GoodsViewModel()
+
     var body: some View {
-        Text("Goods")
+        VStack {
+            Spacer()
+                .frame(height: 1)
+            mainView
+                .background(Color.white)
+        }
+        .frame(width: UIScreen.main.bounds.width)
+        .background(
+            LinearGradient(colors: [Color.green.opacity(0.2), Color.green], startPoint: .leading, endPoint: .trailing)
+                .ignoresSafeArea(.all, edges: .all)
+        )
+        .navigationTitle(Constants.titleText)
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading:
+                                Button(action: {
+            self.presentation.wrappedValue.dismiss()
+        }, label: {
+            Image(systemName: Constants.chevronRight)
+        })
+                                    .foregroundStyle(.gray)
+        )
+        Spacer()
+    }
+
+    @State private var colorTitle: String = Constants.colorText
+    @State private var price: Int = 500
+    @State private var priceSliderColor: Color = .white
+
+    private var mainView: some View {
+        VStack {
+            categoryView
+            pricesView
+            colorsView
+        }
+    }
+
+    @ViewBuilder
+    private var colorsView: some View {
+        Spacer()
+            .frame(height: 45)
+        HStack {
+            Text(colorTitle)
+                .font(.bold(.system(size: 24))())
+                .foregroundStyle(.gray)
+                .padding(.horizontal)
+            Spacer()
+        }
+        if #available(iOS 17.0, *) {
+            LazyVGrid(columns: columnsColorsFilters) {
+                ForEach(viewModel.sourceColors.indices, id: \.self) { indexColor in
+                    Circle()
+                        .stroke(.gray)
+                        .fill(viewModel.sourceColors[indexColor])
+                        .padding(.horizontal)
+                        .onTapGesture {
+                            colorTitle = "Color - \(viewModel.sourceColors[indexColor])"
+                        }
+                }
+            }
+            .padding(.horizontal)
+        }
+    }
+
+    @ViewBuilder
+    private var pricesView: some View {
+        HStack {
+            Text(Constants.pricesText)
+                .font(.bold(.system(size: 24))())
+                .foregroundStyle(.gray)
+                .padding(.horizontal)
+            Spacer()
+        }
+        Slider(value: Binding(
+            get: { Double(self.price) },
+            set: { newValue in
+                self.price = Int(newValue)
+            }
+        ), in: 500...5000, step: 500) {
+            Text(String(price))
+        } onEditingChanged: { newValue in
+            print(newValue)
+        }.onChange(of: price) { newValue in
+            print(newValue)
+        }
+        .padding(.horizontal)
+        .tint(.green)
+        .colorMultiply(.gray)
+        HStack {
+            Text(Constants.minPriceText)
+                .font(.system(size: 15))
+                .foregroundStyle(.gray)
+                .padding(.horizontal)
+            Spacer()
+            Text("\(Int(price))$")
+                .font(.system(size: 15))
+                .foregroundStyle(price == 500 ? .clear : .gray)
+                .padding(.horizontal)
+                .offset(x: getOffsetPriceSlider())
+        }
+    }
+
+    @ViewBuilder
+    private var categoryView: some View {
+        Spacer()
+            .frame(height: 22)
+        HStack {
+            Text(Constants.categoryText)
+                .font(.bold(.system(size: 24))())
+                .foregroundStyle(.gray)
+                .padding(.horizontal)
+            Spacer()
+            Text(Constants.moreText)
+                .font(.bold(.system(size: 24))())
+                .foregroundStyle(.appGray)
+            Image(systemName: Constants.chevronLeft)
+                .foregroundStyle(.appGray)
+                .padding(.trailing)
+        }
+
+        ScrollView(.horizontal) {
+            LazyHGrid(rows: columnsTypeFilters) {
+                ForEach(viewModel.sourceTypes.indices, id: \.self) { indexImage in
+                    RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
+                        .overlay {
+                            Image(viewModel.sourceTypes[indexImage])
+                        }
+                        .frame(width: 120, height: 80)
+                        .foregroundStyle(.appGray)
+                        .shadow(radius: 2, y: 2)
+                        .padding(.leading)
+                }
+            }
+        }
+        .padding(.horizontal)
+    }
+
+    private func getOffsetPriceSlider() -> CGFloat {
+        var offset = price < 4500 ? 33 : 30
+        return -CGFloat((10 - (price/500))) * CGFloat(offset)
     }
 }
 
--- ShopApp/GoodsModule/GoodsView.swift
+++ ShopApp/GoodsModule/GoodsView.swift
@@ -8,27 +8,114 @@
 import SwiftUI
 
 struct GoodsView: View {
-    var body: some View {
-        ZStack {
-            LinearGradient(colors: [Color.green.opacity(0.2), Color.green], startPoint: .leading, endPoint: .trailing)
-                .ignoresSafeArea(.all, edges: .top)
 
-            VStack {
-                Spacer()
-                mainView
+    enum Constants {
+        static let yourPriceText = "Your total price"
+        static let magnifImage = "magnifyingglass"
+        static let seartchText = "Search..."
+        static let filterText = "filter"
+    }
+
+    var body: some View {
+        NavigationView {
+            ZStack {
+                VStack {
+                    searchView
+                    mainView
+                        .background(.white)
+                    Spacer()
+                }
+                .frame(width: UIScreen.main.bounds.width)
             }
-            .frame(width: UIScreen.main.bounds.width)
+        }
+        .navigationBarItems(leading: EmptyView(), trailing: EmptyView())
+
+        .navigationBarHidden(true)
+        .navigationBarBackButtonHidden(true)
+    }
+
+    @State private var searchText = ""
+    @ObservedObject var viewModel = GoodsViewModel()
+
+    @ViewBuilder
+    private var mainView: some View {
+
+        priceView
+        ScrollView {
+            ForEach(viewModel.goods.indices, id: \.self) { goodIndex in
+                GoodCellView(good: viewModel.goods[goodIndex],
+                             indexGood: goodIndex)
+                .environmentObject(viewModel)
+            }
+        }
+        .padding(.top)
+        if viewModel.goods.count == 0 {
+            Spacer()
         }
     }
 
-    private var mainView: some View {
-        VStack {
-           Text("gf")
+    private var priceView: some View {
+        HStack {
+            Spacer()
+            RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
+                .foregroundStyle(.appLightGreen)
+                .frame(width: 350, height: 48)
+                .padding(.trailing, -10)
+                .padding(.top)
+                .overlay(alignment: .center) {
+                    HStack {
+                        Text(Constants.yourPriceText)
+                            .font(.system(size: 24))
+                            .foregroundStyle(.gray)
+                            .padding(.top)
+                            .padding(.trailing)
+                        Text("\(viewModel.goods.map {$0.price * $0.count}.reduce(0,+))$")
+                            .font(.system(size: 24))
+                            .foregroundStyle(.gray)
+                            .padding(.top)
+                    }
+                }
         }
-        .frame(width: UIScreen.main.bounds.width)
-        .background(.white)
+    }
+
+    private var searchView: some View {
+        VStack {
+            ZStack {
+                LinearGradient(colors: [Color.green.opacity(0.2), Color.green], startPoint: .leading, endPoint: .trailing)
+                    .ignoresSafeArea(.all, edges: .top)
+                VStack {
+                    Spacer()
+                    HStack {
+                        RoundedRectangle(cornerSize: CGSize(width: 30, height: 30))
+                            .foregroundStyle(.white)
+                            .overlay(alignment: .leading) {
+                                HStack {
+                                    Image(systemName: Constants.magnifImage)
+                                        .foregroundStyle(.gray)
+                                        .padding(.leading)
+                                    TextField(text: $searchText) {
+                                        Text(Constants.seartchText)
+                                    }
+                                }
+                                .padding(.zero)
+                            }
+                            .frame(width: 312, height: 48)
+                            .padding(.leading)
+                        NavigationLink(destination: FiltersView().environmentObject(viewModel)) {
+                            Image(Constants.filterText)
+                                .padding(.trailing)
+                        }
+
+                    }
+                    .padding(.vertical)
+                }
+            }
+        }
+        .ignoresSafeArea()
+        .frame(height: 90)
     }
 }
+
 
 #Preview {
     GoodsView()
